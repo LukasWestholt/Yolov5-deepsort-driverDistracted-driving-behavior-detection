a5dbd905227ac298293826cfb78249c1f7bac26f
LukasWestholt <support@lukaswestholt.de>
2025-12-05 22:14:24 +0100

    docs: Add example usage to __init__.py

--- src/yolov5_deepsort_driverdistracted_driving_behavior_detection/__init__.py
+++ src/yolov5_deepsort_driverdistracted_driving_behavior_detection/__init__.py
@@ -1,1 +1,8 @@
-import yolov5_deepsort_driverdistracted_driving_behavior_detection.main
+### Example Usage
+# import yolov5_deepsort_driverdistracted_driving_behavior_detection.myframe as myframe
+# import cv2
+#
+# cam = cv2.VideoCapture(0)
+# ok, frame = cam.read()
+# ret, frame_annot = myframe.frametest(frame)
+# print((ret, frame_annot))
